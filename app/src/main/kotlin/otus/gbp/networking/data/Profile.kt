5825c45b73e0a5c7aa7d1f9e2e9f58c767ad3c6a
Nikolay Kochetkov <nkochetkov@jetsmarter.com>
2024-07-15 16:33:44 +0300

    Custom serialization

--- app/src/main/kotlin/otus/gbp/networking/data/Profile.kt
+++ app/src/main/kotlin/otus/gbp/networking/data/Profile.kt
@@ -1,8 +1,14 @@
 package otus.gbp.networking.data
 
 import kotlinx.datetime.Instant
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 
 @Serializable
 data class Profile(
@@ -10,6 +16,19 @@ data class Profile(
     val userId: Int,
     val name: String,
     val age: Int,
+    @Serializable(with = InstantSerializer::class)
     val registered: Instant,
     val interests: List<String> = emptyList()
 )
+
+object InstantSerializer: KSerializer<Instant> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("Instant", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Instant {
+        return Instant.parse(decoder.decodeString())
+    }
+
+    override fun serialize(encoder: Encoder, value: Instant) {
+        encoder.encodeString(value.toString())
+    }
+}
